938678bd413c19781c19a5248cda73d89cff8574
ZenonStella <stellazenon@gmail.com>
2022-08-18 11:19:05 +0200

    exo 5

--- Class/Hero.php
+++ Class/Hero.php
@@ -1,8 +1,42 @@
-<?php 
+<?php
 
-class Hero extends Character {
+class Hero extends Character
+{
     private $_weapon;
-    private $_weaponDomage;
+    private $_weaponDamage;
     private $_shield;
     private $_shieldValue;
+
+    public function setWeapon($weapon)
+    {
+        $this->_weapon = $weapon;
+    }
+    public function getWeapon()
+    {
+        return $this->_weapon;
+    }
+    public function setWeaponDamage($weaponDamage)
+    {
+        $this->_weaponDamage = $weaponDamage;
+    }
+    public function getWeaponDamage()
+    {
+        return $this->_weaponDamage;
+    }
+    public function setShield($shield)
+    {
+        $this->_shield = $shield;
+    }
+    public function getShield()
+    {
+        return $this->_shield;
+    }
+    public function setShieldValue($shieldValue)
+    {
+        $this->_shieldValue = $shieldValue;
+    }
+    public function getShieldValue()
+    {
+        return $this->_shieldValue;
+    }
 }
