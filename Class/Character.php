17108b3a35b121053f877d99b1ceb5b102b1b888
ZenonStella <stellazenon@gmail.com>
2022-08-18 10:01:59 +0200

    exo 2

--- Class/Character.php
+++ Class/Character.php
@@ -1,6 +1,23 @@
-<?php 
+<?php
 
 class Character {
     private $_health;
     private $_rage;
+
+    public function setHealth($health)
+    {
+       $this->_health = $health;
+    }
+    public function getHealth()
+    {
+       return $this->_health;
+    }
+    public function setRage($rage)
+    {
+        $this->_rage = $rage;
+    }
+    public function getRage()
+    {
+        return $this->_rage;
+    }
 }
